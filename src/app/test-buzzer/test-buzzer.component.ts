d587f39cfda781e0c970275c9d4b32abb37183bc
Konstantin Fein <konstantin.fein@fntsoftware.com>
2017-01-18 14:31:10 +0100

    Add click handler

--- src/app/test-buzzer/test-buzzer.component.ts
+++ src/app/test-buzzer/test-buzzer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-test-buzzer',
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestBuzzerComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('buzzer1Red') bz1Red: ElementRef;
+  @ViewChild('buzzer1Orange') bz1Orange: ElementRef;
+  @ViewChild('buzzer1Blue') bz1Blue: ElementRef;
+  @ViewChild('buzzer1Green') bz1Green: ElementRef;
+  @ViewChild('buzzer1Yellow') bz1Yellow: ElementRef;
+
+  @ViewChild('buzzer2Red') bz2Red: ElementRef;
+  @ViewChild('buzzer2Orange') bz2Orange: ElementRef;
+  @ViewChild('buzzer2Blue') bz2Blue: ElementRef;
+  @ViewChild('buzzer2Green') bz2Green: ElementRef;
+  @ViewChild('buzzer2Yellow') bz2Yellow: ElementRef;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.bz1Red.nativeElement.click();
+    }, 1000);
+
+
+    setTimeout(() => {
+      this.bz2Red.nativeElement.click();
+    }, 1000);
+  }
+
+  handleClick() {
+    console.log("Click");
   }
 
 }
