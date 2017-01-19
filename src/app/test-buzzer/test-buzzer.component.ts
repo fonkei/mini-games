2a496659963f3001aa6d3b45955c607c1b6658d2
Konstantin Fein <konstantin.fein@fntsoftware.com>
2017-01-19 07:54:14 +0100

    Add keydown and keyup handler to add a pressed class on buzzer to determine if key are pressed

--- src/app/test-buzzer/test-buzzer.component.ts
+++ src/app/test-buzzer/test-buzzer.component.ts
@@ -1,11 +1,27 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-test-buzzer',
   templateUrl: './test-buzzer.component.html',
-  styleUrls: ['./test-buzzer.component.css']
+  styleUrls: ['./test-buzzer.component.css'],
+  host: {
+    '(window:keydown)': 'handleKeydown($event)',
+    '(window:keyup)': 'handleKeyup($event)'
+  },
 })
-export class TestBuzzerComponent implements OnInit {
+export class TestBuzzerComponent {
+  public static readonly BZ1_RED = 49;
+  public static readonly BZ1_ORANGE = 50;
+  public static readonly BZ1_BLUE = 51;
+  public static readonly BZ1_GREEN = 52;
+  public static readonly BZ1_YELLOW = 53;
+
+  public static readonly BZ2_RED = 54;
+  public static readonly BZ2_ORANGE = 55;
+  public static readonly BZ2_BLUE = 56;
+  public static readonly BZ2_GREEN = 57;
+  public static readonly BZ2_YELLOW = 48;
 
   @ViewChild('buzzer1Red') bz1Red: ElementRef;
   @ViewChild('buzzer1Orange') bz1Orange: ElementRef;
@@ -19,22 +35,88 @@ export class TestBuzzerComponent implements OnInit {
   @ViewChild('buzzer2Green') bz2Green: ElementRef;
   @ViewChild('buzzer2Yellow') bz2Yellow: ElementRef;
 
-  constructor() {
+  bz1RedPressed = false;
+  bz1OrangePressed = false;
+  bz1BluePressed = false;
+  bz1GreenPressed = false;
+  bz1YellowPressed = false;
+  bz2RedPressed = false;
+  bz2OrangePressed = false;
+  bz2BluePressed = false;
+  bz2GreenPressed = false;
+  bz2YellowPressed = false;
+
+  handleKeydown(event) {
+    switch (event.keyCode) {
+      case TestBuzzerComponent.BZ1_RED:
+        this.bz1RedPressed = true;
+        break;
+      case TestBuzzerComponent.BZ1_ORANGE:
+        this.bz1OrangePressed = true;
+        break;
+      case TestBuzzerComponent.BZ1_BLUE:
+        this.bz1BluePressed = true;
+        break;
+      case TestBuzzerComponent.BZ1_GREEN:
+        this.bz1GreenPressed = true;
+        break;
+      case TestBuzzerComponent.BZ1_YELLOW:
+        this.bz1YellowPressed = true;
+        break;
+      case TestBuzzerComponent.BZ2_RED:
+        this.bz2RedPressed = true;
+        break;
+      case TestBuzzerComponent.BZ2_ORANGE:
+        this.bz2OrangePressed = true;
+        break;
+      case TestBuzzerComponent.BZ2_BLUE:
+        this.bz2BluePressed = true;
+        break;
+      case TestBuzzerComponent.BZ2_GREEN:
+        this.bz2GreenPressed = true;
+        break;
+      case TestBuzzerComponent.BZ2_YELLOW:
+        this.bz2YellowPressed = true;
+        break;
+      default:
+        break;
+    }
   }
 
-  ngOnInit() {
-    setTimeout(() => {
-      this.bz1Red.nativeElement.click();
-    }, 1000);
-
-
-    setTimeout(() => {
-      this.bz2Red.nativeElement.click();
-    }, 1000);
+  handleKeyup(event) {
+    switch (event.keyCode) {
+      case TestBuzzerComponent.BZ1_RED:
+        this.bz1RedPressed = false;
+        break;
+      case TestBuzzerComponent.BZ1_ORANGE:
+        this.bz1OrangePressed = false;
+        break;
+      case TestBuzzerComponent.BZ1_BLUE:
+        this.bz1BluePressed = false;
+        break;
+      case TestBuzzerComponent.BZ1_GREEN:
+        this.bz1GreenPressed = false;
+        break;
+      case TestBuzzerComponent.BZ1_YELLOW:
+        this.bz1YellowPressed = false;
+        break;
+      case TestBuzzerComponent.BZ2_RED:
+        this.bz2RedPressed = false;
+        break;
+      case TestBuzzerComponent.BZ2_ORANGE:
+        this.bz2OrangePressed = false;
+        break;
+      case TestBuzzerComponent.BZ2_BLUE:
+        this.bz2BluePressed = false;
+        break;
+      case TestBuzzerComponent.BZ2_GREEN:
+        this.bz2GreenPressed = false;
+        break;
+      case TestBuzzerComponent.BZ2_YELLOW:
+        this.bz2YellowPressed = false;
+        break;
+      default:
+        break;
+    }
   }
-
-  handleClick() {
-    console.log("Click");
-  }
-
 }
