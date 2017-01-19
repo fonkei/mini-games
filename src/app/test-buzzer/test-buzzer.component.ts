c7df5419d0c59871fd5385cfe8f38fb7a1beeb14
Konstantin Fein <konstantin.fein@fntsoftware.com>
2017-01-19 07:55:28 +0100

    Remove unused view childs

--- src/app/test-buzzer/test-buzzer.component.ts
+++ src/app/test-buzzer/test-buzzer.component.ts
@@ -23,18 +23,6 @@ export class TestBuzzerComponent {
   public static readonly BZ2_GREEN = 57;
   public static readonly BZ2_YELLOW = 48;
 
-  @ViewChild('buzzer1Red') bz1Red: ElementRef;
-  @ViewChild('buzzer1Orange') bz1Orange: ElementRef;
-  @ViewChild('buzzer1Blue') bz1Blue: ElementRef;
-  @ViewChild('buzzer1Green') bz1Green: ElementRef;
-  @ViewChild('buzzer1Yellow') bz1Yellow: ElementRef;
-
-  @ViewChild('buzzer2Red') bz2Red: ElementRef;
-  @ViewChild('buzzer2Orange') bz2Orange: ElementRef;
-  @ViewChild('buzzer2Blue') bz2Blue: ElementRef;
-  @ViewChild('buzzer2Green') bz2Green: ElementRef;
-  @ViewChild('buzzer2Yellow') bz2Yellow: ElementRef;
-
   bz1RedPressed = false;
   bz1OrangePressed = false;
   bz1BluePressed = false;
